2b5ad7df237e59df9ed3d83a1ed07c3a451cd3be
andreaSoncco <andrea.soncco.c@gmail.com>
2023-09-05 11:29:04 -0500

    cree la función mdLinks que nos retorna una promesa, configuración del boilerplate para uso de ES MODULES y jest

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-module.exports = () => {
-  // ...
-};
+import fs from 'fs';
+
+export const mdLinks = (path, validate) => {
+  return new Promise((resolve, reject) => {
+    // Identifica si la ruta existe.
+    if(fs.existsSync(path)) {
+    // Chequear si la ruta es absoluta y sino es convertir a una ruta absoluta
+    // Probar si la ruta absoluta es un archivo o un directorio
+    // Si es un directorio filtrar los archivos con extensión .md y pararlos a un arreglo
+    } else {
+    // Si no existe la ruta rechaza la promesa
+    reject('La ruta no existe'); 
+    }
+  })
+}
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,15 @@
-const mdLinks = require('../');
-
+import { mdLinks } from '../index.js';
 
 describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+/*
+  it('Deberia devolver una promesa', () => {
+    expect(mdLinks()).toBe(Promise);
   });
+*/
+  it('Debe rechazar la promesa cuando el path no existe', () => {
+    return mdLinks('https://www.bvl.com.pe/mercados').catch((error) => {
+      expect(error).toBe('La ruta no existe');
+    })
+  })
 
 });
