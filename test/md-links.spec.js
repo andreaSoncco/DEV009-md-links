655f3b9efbb99b63dce29b2b934fb954ce85a9b9
andreaSoncco <andrea.soncco.c@gmail.com>
2023-10-02 18:27:14 -0500

    Agregar un test a la prueba sobre la función mdLinks

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,28 +1,60 @@
 import { mdLinks } from "../mdLinks.js";
 
+
 describe('mdLinks', () => {
   test('debería resolver la promesa con los enlaces', () => {
     const path = './README';
     const validate = false;
 
     return mdLinks(path, validate).then((links) => {
-    expect(links).toEqual([
-      {
-        href: 'https://curriculum.laboratoria.la/es/topics/javascript/03-functions/02-arrow',
-        text: 'Arrow Functions',
-        file: 'prueba.md'
-      },
-      {
-        href: 'https://github.com/markedjs/marked',
-        text: 'marked',
-        file: 'datos.text'
-      },
-      {
-        href: 'https://es.wikipedia.org/wiki/Markdown',
-        text: 'Markdown',
-        file: 'hola.markdown'
-      },
-      ]);
+      expect(links).toEqual(expect.arrayContaining([
+        {
+          href: 'https://curriculum.laboratoria.la/es/topics/javascript/03-functions/02-arrow',
+          text: 'Arrow Functions',
+          file: 'prueba.md'
+        },
+        {
+          href: 'https://github.com/markedjs/marked',
+          text: 'marked',
+          file: 'datos.text'
+        },
+        {
+          href: 'https://es.wikipedia.org/wiki/Markdown',
+          text: 'Markdown',
+          file: 'hola.markdown'
+        }
+      ]));
+    });
+  });
+
+  test('debería resolver la promesa con los enlaces', () => {
+    const path = './README';
+    const validate = true;
+
+    return mdLinks(path, validate).then((links) => {
+      expect(links).toEqual(expect.arrayContaining([
+        {
+          href: 'https://curriculum.laboratoria.la/es/topics/javascript/03-functions/02-arrow',
+          text: 'Arrow Functions',
+          file: 'prueba.md',
+          status: 200,
+          ok: 'ok'
+        },
+        {
+          href: 'https://github.com/markedjs/marked',
+          text: 'marked',
+          file: 'datos.text',
+          status: 200,
+          ok: 'ok'
+        },
+        {
+          href: 'https://es.wikipedia.org/wiki/Markdown',
+          text: 'Markdown',
+          file: 'hola.markdown',
+          status: 200,
+          ok: 'ok'
+        }
+      ]));
     });
   });
 
@@ -39,4 +71,3 @@ describe('mdLinks', () => {
   });
 
 });
-
